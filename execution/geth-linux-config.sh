06e2f60c52cff6e588eaf7dc049f25faeae37f8e
ayout985 <adamlowry788@gmail.com>
2023-10-09 11:44:41 -0700

    commit

--- execution/geth-linux-config.sh
+++ execution/geth-linux-config.sh
@@ -1,6 +1,11 @@
  #use iptables to only allow certain ports
- sudo apt install iptables-persistent
- sudo iptables -P INPUT DROP
- sudo iptables -A INPUT -p tcp -s 192.168.1.68 --dport 22 -j ACCEPT
- iptables-save > /etc/iptables/rules.v4
- 
+sudo apt install iptables-persistent
+sudo iptables -P INPUT DROP
+sudo iptables -P OUTPUT DROP
+sudo iptables -P FORWARD DROP
+sudo iptables -A INPUT -p tcp -s 192.168.1.68 --dport 22 -j ACCEPT
+sudo iptables -A INPUT -p tcp --dport 30303 -j ACCEPT
+sudo iptables -A OUTPUT -p tcp --dport 30303 -j ACCEPT
+sudo iptables -A INPUT -p udp --dport 30303 -j ACCEPT
+sudo iptables -A OUTPUT -p udp --dport 30303 -j ACCEPT
+iptables-save > /etc/iptables/rules.v4
