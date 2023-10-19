797be447d382c97e63a3e42ba9fc558f3297970c
Adam <alowry@Adams-Air.attlocal.net>
2023-10-19 15:28:21 -0400

    sync

--- execution/geth-linux-config.sh
+++ execution/geth-linux-config.sh
@@ -1,8 +1,5 @@
  #use iptables to only allow certain ports
-sudo apt install iptables-persistent
 sudo iptables -P INPUT DROP
-sudo iptables -P OUTPUT DROP
-sudo iptables -P FORWARD DROP
 sudo iptables -A INPUT -p tcp -s 192.168.1.68 --dport 22 -j ACCEPT
 sudo iptables -A INPUT -p tcp --dport 30303 -j ACCEPT
 sudo iptables -A OUTPUT -p tcp --dport 30303 -j ACCEPT
